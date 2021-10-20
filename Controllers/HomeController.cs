5e71cc3bcff25f69ebd463a23dcae3b23d46b6f3
Brandon Razet <brhm_97@hotmail.com>
2021-10-19 19:37:24 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Pc3practica.Models;
+using Pc3practica.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pc3practica.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+  private readonly ApplicationDbContext _context;
+        public HomeController(ApplicationDbContext context)
+        {
+            
+            _context = context;
+        }
+       
+ public async Task<IActionResult> Index()
+        {
+            return View(await _context.DataProductos.ToListAsync());
+        }
+    
+        // GET: Producto/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.DataProductos
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+
+        // GET: Producto/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,Price,ImagenName,Categoria,telefono,Descripcion,NombreC,Lugar")] Productos producto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(producto);
+                await _context.SaveChangesAsync();
+               
+               
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producto);
+        }
+
+        // GET: Producto/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.DataProductos.FindAsync(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: Producto/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Price,ImagenName,Status")] Productos producto)
+        {
+            if (id != producto.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(producto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductoExists(producto.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producto);
+        }
+
+
+        private bool ProductoExists(int id)
+        {
+            return _context.DataProductos.Any(e => e.ID == id);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    
+}
